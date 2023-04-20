085f68b75a2c20ea20206981060c1da20ad10b51
lag369 <lag369@drexel.edu>
2023-04-20 14:55:45 -0400

    trying animation :(

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,32 +1,12 @@
 import { gsap } from "gsap";
+gsap.from("#hero",{duration: 1, delay:0, alpha: 0});
+gsap.to("hero", {duration:1, alpha:1});
 
-
-gsap.set(".logo.",{x:500})
+//gsap.set(".logo.",{x:500})
 
 //const tweenDur = "0.5";
 
-let tweenDur = "0.5";
+//let tweenDur = "0.5";
 
-//^^keyword(let) referencename(tweendur) assignmentoperator value(0.5);
 
-//var is older, avoid using it!
-//let is new var i think?
-//const is also used
 
-//reference name rules:
-    //1. names can contain letters, digits, underscores, adn dollar signs
-    //2. names must begin with a letter or underscore
-    //3. names are case sensitive!!
-    //4. keywords cannot be used as names (i.e. let, any word with tht blue)
-
-//examples
-    //let TweenDuration = "0.5";
-    //let someElement = document.querySelector('.logo');
-    //let allElementsWithClass = document.querySelectorAll('.box');
-    //let mainTl = gsap.timeline();
-
-//GSAP
-
-//gsap.to(target, {duration, vars, ease});
-
-gsap.to("hero", {duration:1, x:200})
