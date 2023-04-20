222fe170daaa8fe6a07df7d553c40f40e5b273d9
lag369 <lag369@drexel.edu>
2023-04-20 15:20:43 -0400

    animation fail :(

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,12 +1,19 @@
 import { gsap } from "gsap";
-gsap.from("#hero",{duration: 1, delay:0, alpha: 0});
-gsap.to("hero", {duration:1, alpha:1});
+
+function Hmotion(){
+gsap.set("#hero", {alpha: 0})
+var tl = gsap.timeline()
+tl.to("#hero", { duration:3, alpha:1}, "trigger");
+return tl ;
+}
+var mainTl = gsap.timeline();
+mainTl.add(Hmotion)()
+; 
+
+//gsap.to("hero", {duration:1, alpha:1});
 
 //gsap.set(".logo.",{x:500})
 
 //const tweenDur = "0.5";
 
 //let tweenDur = "0.5";
-
-
-
