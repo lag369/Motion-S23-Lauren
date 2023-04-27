4e6471160908b033f123e937fef31e9c141bf76d
lag369 <lag369@drexel.edu>
2023-04-27 16:05:34 -0400

    added tl!

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,14 +1,14 @@
 import {gsap} from "gsap";
 
-function Hmotion(){
-gsap.set("#hero", {alpha: 0})
-var tl = gsap.timeline()
-tl.to("#hero", { duration:3, alpha:1}, "trigger");
-return tl ;
-}
-var mainTl = gsap.timeline();
-mainTl.add(Hmotion)()
-; 
+// function Hmotion(){
+// gsap.set("#hero", {alpha: 0})
+// var tl = gsap.timeline()
+// tl.to("#hero", { duration:3, alpha:1}, "trigger");
+// return tl ;
+// }
+// var mainTl = gsap.timeline();
+// mainTl.add(Hmotion)()
+// ; 
 
 //gsap.to("hero", {duration:1, alpha:1});
 
@@ -18,6 +18,6 @@ mainTl.add(Hmotion)()
 
 //let tweenDur = "0.5";
 
-gsap.to("#hero-content", { duration:2.5, ease:"back.out(1.7)", y: -500 });
+gsap.from("#hero-content", { duration:2.5, ease:"back.out(1.7)", y: -500 });
 
 gsap.from(".menuItem", {duration:1, alpha:0, stagger:0.25, y:"-=25", delay:1})
